1245ff303afa27d0a23437332eb74a151f2b12e0
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@etu.univ-grenoble-alpes.fr>
2021-06-14 19:01:23 +0200

    commit Julian

--- Automata/src/Tests/TestParseur.java
+++ Automata/src/Tests/TestParseur.java
@@ -1,5 +1,29 @@
 package Tests;
 
+import java.util.List;
+
+import Model.automata.ast.AST;
+import Model.automata.ast.Automaton;
+import Model.automata.creation.AstToObject;
+import Model.automata.parser.AutomataParser;
+
 public class TestParseur {
+
+	List<Automaton> loadAutomata(String filename) {
+	    try {
+	      AST ast = (AST) AutomataParser.from_file(filename);
+	      //...
+	      // TODO à vous de constuire les automates à partir de l'AST
+	      //...
+	      Automaton automata = ast.accept(new AstToObject());
+	      return automata ;
+	    } catch (Exception ex) {
+	      return null;
+	    }
+	  }
+
+	public static void main() {
+		loadAutomata("");
+	}
 
 }

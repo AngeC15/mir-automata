ec156729fc18992c1b2bc4ffbdf89bf00361ef01
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@etu.univ-grenoble-alpes.fr>
2021-06-14 12:06:25 +0200

    Modification pour automaton, utilisation de classes StateExtension en substitution des id en int, reste l'integration dans d'autres classes à faire

---
+++ Automata/src/Model/automata/conditions/operators/OrOperator.java
@@ -0,0 +1,28 @@
+package Model.automata.conditions.operators;
+
+import Model.Entity;
+import Model.automata.conditions.Condition;
+
+public class OrOperator extends Condition{
+	//on utilise des operateurs ici à gauche et à droite
+		public Condition condDroite;
+		public Condition condGauche;
+		
+		public OrOperator(Object condDroite, Object condGauche) throws Exception {
+			super();
+			try {
+			this.condDroite = (Condition) condDroite;
+			this.condGauche = (Condition) condGauche;
+			}catch(Exception e) {
+				System.out.println("Erreur de changement de type de variable");
+				throw new Exception("Erreur, in orOperator");
+			}
+		}
+
+		@Override
+		public boolean eval(Entity entity) {
+			return (condDroite.eval(entity) || condGauche.eval(entity));
+		}
+
+		
+}
---
+++ Automata/src/Model/automata/creation/StateExtension.java
@@ -0,0 +1,14 @@
+package Model.automata.creation;
+
+public class StateExtension {
+
+		public String name;
+
+		public StateExtension(String name) {
+			super();
+			this.name = name;
+		}
+		
+		
+
+}

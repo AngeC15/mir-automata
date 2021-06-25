a24c45ce33755cd57673174cea3bf25900a27bff
joan <joan.besante@etu.univ-grenoble-alpes.fr>
2021-06-25 16:33:39 +0200

    weapon_generator fini

--- Automata/src/Model/entities/weapon/enum_weapon.java
+++
@@ -1,5 +0,0 @@
-package Model.entities.weapon;
-
-public enum enum_weapon {
-AssaultRifle,Axe,Croissant,Guisarm,Halberd,Hammer,Juliette,Longsword,MachineGun,Pistol,Revolver,Romeo,Scythe,Spear,Sword,TommyGun;
-}
--- Automata/src/Model/monster_generator/Weapon_cover.java
+++ Automata/src/Model/monster_generator/Weapon_cover.java
@@ -8,10 +8,7 @@ import java.util.List;
 import Model.World;
 import Model.entities.Entity;
 import Model.entities.enemies.Plane;
-import Model.entities.weapon.AssaultRifle;
 import Model.entities.weapon.*;
-import Model.entities.weapon.Weapon;
-import Model.entities.weapon.enum_weapon;
 import Model.loader.AutomataLoader;
 import Model.loader.TemplatesLoader;
 import Model.physics.ColliderType;
@@ -23,20 +20,66 @@ import Model.physics.primitives.Square;
 import View.Avatar;
 import View.Template;
 
-public class  Weapon_cover extends Entity {
+public class Weapon_cover extends Entity {
 	Weapon w;
-	
-	public Weapon_cover(int nb_weapon,World world) throws IOException {
-		
+
+	public Weapon_cover(int nb_weapon,World world,double x,double y) throws IOException {
 		super(AutomataLoader.get("Dead"), 3);
 		HitBox h = new HitBox();
 		h.add(new PrimitiveInstance(new Square(), AffineTransform.getScaleInstance(3.1f, 5.2f)));
 		this.body = new PhysicsBody(h, ColliderType.Area, 15.0f, 40.0f, this);
-		w=new TommyGun("Bullet");
-//		enum_weapon weapon = enum_weapon.values()[9];
+		
+		switch (nb_weapon) {
+		case 0:
+			w=new Axe();
+			break;
+		case 1:
+			w=new Croissant();
+			break;
+		case 2:
+			w=new Guisarm();
+			break;
+		case 3:
+			w=new Halberd();
+			break;
+		case 4:
+			w=new Juliette("Bullet");
+			break;
+		case 5:
+			w=new Longsword();
+			break;
+		case 6:
+			w=new MachineGun("Bullet");
+			break;
+		case 7:
+			w=new Pistol("Bullet");
+			break;
+		case 8:
+			w=new Revolver("Bullet");
+			break;
+		case 9:
+			w=new Romeo();
+			break;
+		case 10:
+			w=new Scythe();
+			break;
+		case 11:
+			w=new Shotgun("Bullet");
+			break;
+		case 12:
+			w=new Sniper("Bullet");
+			break;
+		case 13:
+			w=new Sword();
+			break;
+		case 14:
+			w=new TommyGun("Bullet");
+			break;
+		}
+		
 		Template tmpPlane = TemplatesLoader.get("Plane");
 		new Avatar(this, tmpPlane);
-		this.getTransform().concatenate(AffineTransform.getTranslateInstance(10, 10));
+		this.getTransform().concatenate(AffineTransform.getTranslateInstance(x,y));
 		world.addEntity(this);
 		
 	}
@@ -48,5 +91,5 @@ public class  Weapon_cover extends Entity {
 	public void setW(Weapon w) {
 		this.w = w;
 	}
-	
+
 }

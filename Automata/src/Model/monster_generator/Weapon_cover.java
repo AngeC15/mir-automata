7c61aadaf8086c834fb812e92b75ab07c95eba80
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@etu.univ-grenoble-alpes.fr>
2021-06-25 14:53:44 +0200

    ajout d'une arme aléatoire lorsqu'une box spawn

--- Automata/src/Model/monster_generator/Weapon_cover.java
+++ Automata/src/Model/monster_generator/Weapon_cover.java
@@ -4,14 +4,12 @@ import java.awt.geom.AffineTransform;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import Model.World;
 import Model.entities.Entity;
 import Model.entities.enemies.Plane;
-import Model.entities.weapon.AssaultRifle;
 import Model.entities.weapon.*;
-import Model.entities.weapon.Weapon;
-import Model.entities.weapon.enum_weapon;
 import Model.loader.AutomataLoader;
 import Model.loader.TemplatesLoader;
 import Model.physics.ColliderType;
@@ -24,21 +22,80 @@ import View.Avatar;
 import View.Template;
 
 public class  Weapon_cover extends Entity {
-	Weapon w;
+	protected Weapon w;
+	protected ArrayList<Integer> numeroArme;
 	
 	public Weapon_cover(int nb_weapon,World world) throws IOException {
 		
 		super(AutomataLoader.get("Dead"), 3);
+		
+	
+		numeroArme = new ArrayList<Integer>();
+		
 		HitBox h = new HitBox();
 		h.add(new PrimitiveInstance(new Square(), AffineTransform.getScaleInstance(3.1f, 5.2f)));
 		this.body = new PhysicsBody(h, ColliderType.Area, 15.0f, 40.0f, this);
-		w=new TommyGun("Bullet");
-//		enum_weapon weapon = enum_weapon.values()[9];
+		w = randomWeapon(); 
 		Template tmpPlane = TemplatesLoader.get("Plane");
 		new Avatar(this, tmpPlane);
 		this.getTransform().concatenate(AffineTransform.getTranslateInstance(10, 10));
 		world.addEntity(this);
 		
+	}
+
+	private Weapon randomWeapon() {
+		int nbItems = enum_weapon.values().length;
+	    Random r = new Random();
+        int random = r.nextInt(18);
+        enum_weapon weaponSel = enum_weapon.values()[random];
+        Weapon weaponToRet = WeaponInTheBox(weaponSel);
+		//System.out.println("\n " + weaponToRet.toString());
+		return weaponToRet;
+	}
+
+	private Weapon WeaponInTheBox(enum_weapon weaponSel) {
+		Weapon toRet = null;
+		switch (weaponSel) {
+		case AssaultRifle :
+			return new AssaultRifle("Bullet");
+		case Axe :
+			return new Axe();
+		case Croissant :
+			return new Croissant();
+		case Guisarm :
+			return new Guisarm();
+		case Halberd :
+			return new Halberd();
+		case Hammer :
+			return new Hammer();
+		case Juliette :
+			return new Juliette("Bullet");
+		case Longsword :
+			return new Longsword();
+		case MachineGun :
+			return new MachineGun("Bullet");
+		case Pistol :
+			return new Pistol("Bullet");
+		case Revolver :
+			return new Revolver("Bullet");
+		case Romeo :
+			return new Romeo();
+		case Scythe :
+			return new Scythe();
+		case Shotgun :
+			return new Shotgun("Bullet");
+		case Sniper :
+			return new Sniper("Bullet");
+		case Spear :
+			return new Spear();
+		case Sword :
+			return new Sword();
+		case TommyGun :
+			return new TommyGun("Bullet");
+		default:
+			System.out.println("Erreur reconnaissance de l'arme aléatoire dans Weapon_cover");
+			return null;
+		}
 	}
 
 	public Weapon getW() {
